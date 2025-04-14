65070a482439a9a909789149d4670368de460e2b
Bailey Kane <bailey.orion.kane@gmail.com>
2025-04-14 18:25:36 -0400

    Clean up CSS to avoid redundant / colliding styles with global CSS sheet

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,10 +25,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col bg-gray-50 dark:bg-gray-900`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}
       >
-        <main className="flex-grow">{children}</main>
-        <footer className="w-full py-4 text-center text-sm text-gray-600 dark:text-gray-400 border-t border-gray-200 dark:border-gray-700">
+        <main className="flex-1 bg-gray-50">{children}</main>
+        <footer
+          className="w-full py-4 text-center text-sm text-gray-600 dark:text-gray-400
+        bg-gray-100 dark:bg-gray-950 border-t border-gray-300 dark:border-gray-700"
+        >
           Built by{" "}
           <a
             href="https://www.baileykane.co?ref=supplylibrary"
