fd0b66d0b4b67b17ed5c51a9439326ab1031a5d0
Bailey Kane <bailey.orion.kane@gmail.com>
2025-05-14 23:11:29 -0400

    Remove unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 // import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
-import Link from "next/link";
-import { Button } from "@/components/ui/button";
 import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
 
